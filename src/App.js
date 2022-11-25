7f1a815a95951a53f05f06418261b5f20c897a32
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2022-11-25 15:24:50 +0100

    still working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home"
-import About from "./components/About/About"
-import Navbar from "./components/Navbar"
-=======
-import Aboutsection from "./components/Aboutsection";
-import Topsection from "./components/Topsection";
-import Aboutcenter from "./components/Aboutcenter";
-import Aboutbottom from "./components/Aboutbottom";
-import Aboutending from "./components/Aboutending";
-import Servicetop from "./components/Servicetop";
-import Servicemiddle from "./components/Servicemiddle";
-import Servicebottom from "./components/Servicebottom";
-
->>>>>>> 566b5d46aebfb75d8c5910414437bae9c0b41553
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Navbar from "./components/Navbar";
+import Footersection from "./components/Footersection";
+import Copyright from "./components/Copyright";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-    </Routes>
-=======
-      <Topsection />
-      <Aboutsection />
-      <Aboutcenter />
-      <Aboutbottom />
-      <Aboutending />
-      <Servicetop />
-      <Servicemiddle />
-      <Servicebottom />
->>>>>>> 566b5d46aebfb75d8c5910414437bae9c0b41553
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footersection />
+      <Copyright />
     </>
   );
 }
